317c7bfa2dade3c3d118c25faba0e2aa7102242d
Pardeep Chaurasiya <chaurasiyapardeep001@gmail.com>
2023-06-03 22:06:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import data from "./components/data/data.json"
 
 function App() {
+
+  let [itemCount,setItemCount] = useState(0)
+
+  const manageItem =(e) =>{
+    if(e.target.innerText==="Add to cart")
+      addItem(e)
+    else if(e.target.innerText === "Remove from Cart")  
+      removeItem(e)
+  }
+
+  const addItem = (e) =>{
+    setItemCount(()=> itemCount+1)
+    e.target.innerText = "Remove from Cart"
+    e.target.classList.value = "btn btn-danger mt-auto"
+  }
+  
+  const removeItem = (e) =>{
+    setItemCount(()=> itemCount-1)
+    e.target.innerText = "Add to Cart"
+    e.target.classList.value = "btn btn-primary mt-auto"
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Navbar itemCount={itemCount}/>
+   <Header/>
+   <Card data={data} manageItem={manageItem} />
+   <Footer/>
+   </>
   );
 }
 
